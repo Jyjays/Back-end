4ce2a19fb78db01b7c588d896e80a2a3b64a519d
Jyjays <jiang13475384640@outlook.com>
2023-02-18 15:37:21 +0800

    Initial commit

---
+++ src/main/java/com/jyjays/BackEndAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.jyjays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jyjays/BackEndAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyjays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
